7cab3607f2894819dc409c9f37c59e8390698b29
Bryan Wu <bryanwu@berkeley.edu>
2020-07-21 01:06:37 -0700

    add requirementSet gui aside from requirementCategories

--- gui/src/RequirementSet.js
+++ gui/src/RequirementSet.js
@@ -1,31 +1,88 @@
 import React, { Component } from 'react';
 import { Form } from 'react-bootstrap';
+import RequirementCategory from './RequirementCategory'
+//https://www.sicara.ai/blog/2018-06-27-custom-nested-validated-forms-with-react please just copy this code
 class RequirementSet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateId = this.updateId.bind(this);
+  }
+  state = {
+    id: '',
+    name: '',
+    parentId: '',
+    type: 'major',
+    RequirementCategories: []
+  };
+  updateId = (newId) => {
+    this.setState({
+      ...this.state,
+      id: newId.target.value,
+    })
+  }
+  updateName = (newName) => {
+    this.setState({
+      ...this.state,
+      name: newName.target.value,
+    })
+  }
+  updateParentId = (newId) => {
+    this.setState({
+      ...this.state,
+      parentId: newId.target.value,
+    })
+  }
+  updateType = (newType) => {
+    this.setState({
+      ...this.state,
+      type: newType.target.value,
+    })
+  }
   render() {
     return (
-      <Form>
-      <Form.Group controlId="RequirementSet">
-        <Form.Label>ID:</Form.Label>
-        <Form.Text className="text-muted">
-        </Form.Text>
-        <Form.Control size="lg" type="text" placeholder="bioengineering" />
-      </Form.Group>
-      <Form.Group controlId="exampleForm.ControlInput1">
-        <Form.Label>Name:</Form.Label>
-        <Form.Text className="text-muted">
-          i.e. "Linguistics Major"
-        </Form.Text>
-        <Form.Control />
-      </Form.Group>
-      <Form.Group controlId="exampleForm.ControlSelect1">
-        <Form.Label>Type:</Form.Label>
-        <Form.Control as="select">
-          <option>major</option>
-          <option>minor</option>
-          <option>other</option>
-        </Form.Control>
-      </Form.Group>
-    </Form>
+      <form>
+        <div>
+          <label for="id">id: </label>
+          <input
+            name="id"
+            type="text"
+            value={this.state.id}
+            onChange={this.updateId}
+          />
+        </div>
+        <div>
+          <label for="name">name: </label>
+          <input
+            name="name"
+            type="text"
+            value={this.state.name}
+            onChange={this.updateName}
+          />
+        </div>
+        <div>
+          <label for="parentId">parentId: </label>
+          <input
+            name="parentId"
+            type="text"
+            value={this.state.parentId}
+            onChange={this.updateParentId}
+          />
+        </div>
+        <div>
+          <label for="type">type: </label>
+          <select
+            name="type"
+            value={this.state.type}
+            onChange={this.updateType}
+          >
+          <option value="major">major</option>
+          <option value="minor">minor</option>
+          <option value="other">other</option>
+          </select>
+        </div>
+
+
+      </form>
   );
   }
 }
