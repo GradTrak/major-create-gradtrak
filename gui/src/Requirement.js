0a9151a57bfcacf3f34acceff321f8c036a4a482
Bryan Wu <bryanwu@berkeley.edu>
2020-08-03 22:05:10 -0700

    Add wrapper to Requirements

--- gui/src/Requirement.js
+++ gui/src/Requirement.js
@@ -10,10 +10,70 @@ class Requirement extends React.Component {
     data[field] = event.target.value;
     this.props.onChange({target:{value: data}});
   }
+  updateRequirement = (event) => {
+    let data = {...event.target.value};
+    this.props.onChange({target:{value: data}})
+  }
   render() {
+    let requirementComponent;
+    const req = this.props.value;
+    switch (req.type) {
+      case 'course':
+        requirementComponent = <CourseRequirement
+        value= {req}
+        onChange={this.updateRequirement}
+      />
+        break;
+      case 'multi':
+        requirementComponent = <MultiRequirement
+        value= {req}
+        onChange={this.updateRequirement}
+      />
+        break;
+      case 'poly':
+        requirementComponent = <PolyRequirement
+        value= {req}
+        onChange={this.updateRequirement}
+      />
+        break;
+      case 'tag':
+        requirementComponent = <TagRequirement
+        value= {req}
+        onChange={this.updateRequirement}
+      />
+        break;
+      default:
+        requirementComponent = <Requirement
+        value= {req}
+        onChange={this.updateRequirement}
+        />
+    };
     return (
-      <p>Requirement</p>
-  );
+      <div>
+        <div>
+          <label for="type">type: </label>
+          <select
+            name="type"
+            value={this.props.value.type}
+            onChange={this.handleFieldChange('type')}
+          >
+          <option value="course">course</option>
+          <option value="multi">multi</option>
+          <option value="poly">poly</option>
+          <option value="tag">tag</option>
+          </select>
+        </div>
+          <label for="name">Requirement Name: </label>
+          <input
+            name="name"
+            type="text"
+            value={this.getProp('name')}
+            onChange={this.handleFieldChange('name')}
+          />
+          {requirementComponent}
+        </div>
+      )
+    );
   }
 }
 
